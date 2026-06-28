05fb788b58f6eb3c6fad7074b78517ddf73c09c0
Sandeep <sandeep_d5@hotmail.com>
2026-06-27 17:33:37 -0700

    Created a data file

---
+++ src/data/index.tsx
@@ -0,0 +1,9 @@
+export const PERSONAL_INFO = {
+  name: "Sandeep Dhillon",
+  role: "Computer Science Student",
+  email: "sda183@sfu.ca",
+  github: "https://github.com/Tillloo",
+  linkedin: "https://www.linkedin.com/mynetwork/grow/",
+  location: "Vancouver, Canada"
+};
+
